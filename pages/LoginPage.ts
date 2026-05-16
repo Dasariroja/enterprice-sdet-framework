c1276695b28816828c7a3cacac6a993e1f55a912
Team-testum <testers.testum@gmail.com>
2026-05-16 14:12:25 +0100

    updated comments

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -4,13 +4,13 @@ export class LoginPage {
   readonly passwordInput: Locator
   readonly loginButton: Locator;
 
-  constructor(private page: Page) {
+  constructor(page: Page) {
     this.usernameInput = page.locator('[data-test="username"]');
     this.passwordInput = page.locator('[data-test="password"]');
     this.loginButton = page.locator('[data-test="login-button"]');
   }
 
-  async login(username:string, password:string) {
+  async login(username: string, password: string) {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.loginButton.click();
